1abe318893148fcfa27224880b1a7fb33f337036
Jenn-Torres <jennifer.rosalia.gibert@gmail.com>
2023-04-27 15:21:02 +0100

    Added new component Photos and sheCodes Url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
    setResults(response.data[0]);
   }
 
+  function handleSheCodesResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let sheCodesApiKey = "481ddobef3865a588e70t2ab3c3ada10";
+    let sheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${sheCodesApiKey}`;
+    axios.get(sheCodesApiUrl).then(handleSheCodesResponse);
   }
 
   function handleSubmit(event) {
@@ -49,6 +60,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
